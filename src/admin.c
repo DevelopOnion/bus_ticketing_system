2fd013501c67bafd77858a1aa70786ac99748159
DevelopOnion <smeyonion.nou@gmail.com>
2025-06-26 13:40:55 +0700

    Add funciton for admin

---
+++ src/admin.c
@@ -0,0 +1,39 @@
+#include "admin.h"
+#include "util.h"
+
+void showAdminMenu() {
+    int option;
+    do {
+        printf("Admin menu:\n");
+        printf("1. Manage buses\n");
+        printf("2. View all booking\n");
+        printf("3. Log out\n");
+        printf("Enter option: ");
+        scanf("%d, &option");
+        Util_clearInputBuffer();
+
+        switch (option) {
+            case 1:
+                manageBuses();
+                break;
+            case 2:
+                viewAllBookings();
+                break;
+            case 3:
+                adminLogout();
+                break;
+            default:
+                printf("Invalid option.\n");
+        }
+
+    } while (option != 3);
+}
+
+void manageBuses() {
+    /* 
+    * addBus()
+    * editBus()
+    * deleteBus()
+	* viewBuses()
+    */
+}
---
+++ src/admin.h
@@ -0,0 +1,14 @@
+#ifndef ADMIN_H
+#define ADMIN_H
+
+/**
+ * Description: Display all of the option for admin menu
+ * Paramater: None
+ * Return: void
+ */
+void showAdminMenu();
+void manageBuses();
+void viewAllBookings();
+void adminLogout();
+
+#endif
